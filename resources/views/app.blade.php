a3a69a5df000d1f122035cc113c9ea253aa28150
Toti Cuervo <cuervor14@gmail.com>
2019-05-13 21:12:02 -0500

    final update 2.3

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,7 +16,7 @@
 
 
     <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet" type="text/css">
-    {{--<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">--}}
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
           integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
